9567a60e9751808544a2b89982924817cc19beab
Daniel Mesa <damesamarin@gmail.com>
2021-08-12 17:55:26 +0200

    formularios reactivos, imagenes usuario por defecto Rick y morty

--- src/app/components/Persona/nueva-persona/nueva-persona.component.ts
+++ src/app/components/Persona/nueva-persona/nueva-persona.component.ts
@@ -20,8 +20,19 @@ export class NuevaPersonaComponent implements OnInit{
 
   ngOnInit(): void {
     this.personaActualizar=this.service.persona_actualizar;
-    if(this.router.url==='/actualizar')
-    this.personaForm.setValue(this.personaActualizar)
+    if(this.router.url==='/actualizar'){
+      this.personaForm.get('user').setValue(this.personaActualizar.user);
+      this.personaForm.get('password').setValue(this.personaActualizar.password);
+    this.personaForm.get('surname').setValue(this.personaActualizar.surname);
+     this.personaForm.get('company_email').setValue(this.personaActualizar.company_email);
+      this.personaForm.get('personal_email').setValue(this.personaActualizar.personal_email);
+    this.personaForm.get('city').setValue(this.personaActualizar.city);
+     this.personaForm.get('active').setValue(this.personaActualizar.active);
+      this.personaForm.get('created_Date').setValue(this.personaActualizar.created_Date);
+      this.personaForm.get('imagen_url').setValue(this.personaActualizar.imagen_url);
+    }
+    
+    
   }
   /*variables*/
   personaActualizar:PersonaDTO;
@@ -44,6 +55,8 @@ export class NuevaPersonaComponent implements OnInit{
     this.personaForm.get('surname').value, this.personaForm.get('company_email').value, this.personaForm.get('personal_email').value,
     this.personaForm.get('city').value, this.personaForm.get('active').value, this.personaForm.get('created_Date').value,this.personaForm.get('imagen_url').value);
 
+    persona=this.imagenpordefecto(persona);
+
     this.service.crearPersona(persona).subscribe(()=>this.router.navigate(['/']));  
 
   }
@@ -53,11 +66,14 @@ export class NuevaPersonaComponent implements OnInit{
     let persona=new PersonaDTO(this.personaActualizar.id,this.personaForm.get('user').value,this.personaForm.get('password').value,
     this.personaForm.get('surname').value, this.personaForm.get('company_email').value, this.personaForm.get('personal_email').value,
     this.personaForm.get('city').value, this.personaForm.get('active').value, this.personaForm.get('created_Date').value,this.personaForm.get('imagen_url').value);
-
+    persona=this.imagenpordefecto(persona);
     this.service.actualizar(persona).subscribe((actualizar)=>{
       this.service.contador_alertas=this.service.contador_alertas+1;
-      this.service.alertas.push(new Alerta("Actualizacion","Persona con id:"+persona.id+" ha sido actualizado"))});
-     this.router.navigate(['/listar'])
+      this.service.alertas.push(new Alerta("Actualizacion","Persona con id:"+persona.id+" ha sido actualizado"));
+      
+    }).unsubscribe();
+    this.router.navigate(['/listar'])
+     
   }
   borrarPersona(persona:PersonaDTO):void{
     this.service.borrarPersona(persona.id).subscribe((borrado)=>{
@@ -81,6 +97,20 @@ export class NuevaPersonaComponent implements OnInit{
       'active':[false],
       'created_Date':['']
     })
+  }
+
+  imagenpordefecto(comprobar:PersonaDTO):PersonaDTO{
+    let random=Math.floor(Math.random()*600+1);
+    let url=`https://rickandmortyapi.com/api/character/avatar/${random}.jpeg`;
+    if(comprobar.imagen_url.length<=1){
+      let devolver=comprobar;
+      devolver.imagen_url=url
+
+      return devolver;
+
+    }
+    return comprobar;
+    
   }
 
   
